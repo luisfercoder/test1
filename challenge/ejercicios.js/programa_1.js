38360524f7a1ba46ae92a4b011f325d559e78c9c
Luis Fernando <luisfercoder@gmail.com>
2024-04-11 14:45:11 -0600

    Different ways to solve a problem

--- challenge/ejercicios.js/programa_1.js
+++ challenge/ejercicios.js/programa_1.js
@@ -1,20 +1,40 @@
 
 //Programa 1
-function counter(texto){
-  if(typeof texto==='string'){
-    console.log(texto.length);
-  }else{
-    console.log('El valor ingresado no es un string');
-  }
+// function counter(texto){
+//   if(typeof texto==='string'){
+//     console.log(texto.length);
+//   }else{
+//     console.log('El valor ingresado no es un string');
+//   }
   
-}
- counter('Hola mundo');
+// }
+//  counter('Hola mundo');
+//usando arrow function 
+// const  contarCaracter= (cadena="")=>
+//   (!cadena )
+//   ?console.warn("No ingresaste una cadena")
+//   :console.info(`La cadena ${cadena} , tiene ${cadena.length} caracteres`)
+
+//   contarCaracter(true);
+
 
 // //Programa 2
 // function textCutter(texto2,characters){
 //   console.log(texto2.slice(0,characters));
 // }
 // textCutter('Hola Mundo',4)
+
+
+// //con arrow functions
+// const textCut = (cadena="",longitud)=>
+//   (!cadena)
+//    ?console.warn('No ingresaste una cadena')
+//    :(longitud==undefined)
+//    ?console.warn('No ingresaste una longitud')
+//    :console.info(cadena.slice(0,longitud));
+
+//   textCut("Hola mundo",5)
+
 // //Programa 3
 // function createArray(text3){
 //   let array=(text3.split(" "));
@@ -23,8 +43,31 @@ function counter(texto){
 //  }
 // createArray('Hola que tal ');
 
+
+//con arrow funtion 
+// const array =(text="",separador=undefined)=>
+//  (!text)
+//  ?console.warn('No es una cadena valida')
+//  :(separador==undefined)
+//   ?console.warn('No es una separador valido')
+//   :console.info(text.split(" "));
+
+// array('Hola mundo como estas',' ')
+
 //Programa 4
 // function finalText(texto4,times){
 //   console.log(texto4.repeat(times));
 // }
 // finalText('Hola Mundo ',3)
+ 
+//con arrow
+// const repeat =(phrase="",repeatTime)=>
+// (!phrase)
+//  ?console.warn('No es una frase valida')
+//  :(!isNaN(repeatTime))
+//    ?console.info(phrase.repeat(repeatTime))
+//    :console.warn('No es un numero  valido');
+  
+
+// repeat('Hola como estas',0)
+
